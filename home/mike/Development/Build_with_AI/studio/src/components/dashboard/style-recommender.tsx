34dc0b4d5c89d8f3e1e4ec5c46913a0ac2090799
Mike <mo664040@gmail.com>
2025-11-28 07:18:11 +0000

    didnt see the changes, the app is still in the same state as before

--- home/mike/Development/Build_with_AI/studio/src/components/dashboard/style-recommender.tsx
+++ home/mike/Development/Build_with_AI/studio/src/components/dashboard/style-recommender.tsx
@@ -1,3 +1,4 @@
+// src/components/dashboard/style-recommender.tsx
 "use client";
 
 import { useState } from "react";
--- home/mike/Development/Build_with_AI/studio/src/components/dashboard/virtual-try-on.tsx
+++ home/mike/Development/Build_with_AI/studio/src/components/dashboard/virtual-try-on.tsx
@@ -1,3 +1,4 @@
+// src/components/dashboard/virtual-try-on.tsx
 "use client";
 
 import { useState, useEffect } from "react";
